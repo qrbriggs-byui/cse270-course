3ba00d4d7948995a0df87bc19152e760fcb15032
qrbriggs-byui <briggsq@byui.edu>
2023-06-09 23:20:56 -0600

    Update to singleton

--- directorydata_service/data/views.py
+++ directorydata_service/data/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+import time
+import random
 
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    response = """{
+response = """{
     "businesses": [
         {
             "name":"Teton Elementary",
@@ -98,8 +99,13 @@ def index(request):
         }                
     ]
 }"""
-    return HttpResponse(response, content_type="application/json",headers={
+
+headers = {
 "Cross-Origin-Opener-Policy":"unsafe-none",
 'Access-Control-Allow-Origin':'*',
-'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept'})
+'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept'
+}
+
+def index(request):
+    return HttpResponse(response, content_type="application/json", headers=headers)
     
