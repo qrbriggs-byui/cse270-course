22afd239077ef2b0cb31f994db3e98f76e71eaff
qrbriggs-byui <briggsq@byui.edu>
2024-02-27 16:36:28 -0700

    Update 1.2 and 1.3 releases

--- teton/1.3/scripts/main.js
+++ teton/1.3/scripts/main.js
@@ -1,13 +1,8 @@
-// Show the meet-greet message on Tue/Thu
-// Date.getDay() uses a 0 based index to return the day of the week
-var messagedate = new Date();
-if (messagedate.getDay() == 2 || messagedate.getDay() == 4) {
-  document.querySelector("#meet-greet").classList.add("active");
-}
-
 // select HTML elements in the document
 const weatherIcon = document.querySelector("#weathericon");
 const weatherDesc = document.querySelector("#weatherdesc");
+const weatherTemp = document.querySelector("#temperature");
+const weatherWind = document.querySelector("#windspeed");
 
 function displayResults(weatherData) {
   // You can use @2x or @4x to make the icon bigger, or omit it for the standard size
@@ -18,6 +13,8 @@ function displayResults(weatherData) {
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
   weatherDesc.textContent = main; 
+  weatherTemp.textContent = weatherData.main.temp.toFixed(0);
+  weatherWind.textContent = weatherData.wind.speed.toFixed(0);  
 }
 
 async function getTheWeather() {
@@ -35,38 +32,3 @@ async function getTheWeather() {
 }
 
 getTheWeather();
-
-function displaySpotlights(businessList){
-  spotlights = []
-  for (let i=0; i < 3; i++){
-    var elt = Math.floor(Math.random() * businessList.length)
-    spotlights.push(businessList.splice(elt, 1)[0]);
-  }
-
-  // Now display stuff  
-  var mainspotlight = document.querySelector('.main-spotlight');
-  spotlightcount = 1;
-  spotlights.forEach((spotlight) => {
-    var newdiv = document.createElement('div');
-    newdiv.classList.add('spotlight'+spotlightcount);
-    spotlightcount++;
-    newdiv.innerHTML = `<h4>${spotlight.name}</h4>
-                        <p class="centered-image"><a href="${spotlight.websiteURL}"><img src="${spotlight.imageURL}"></a></p>
-                        <p>${spotlight.streetAddress}, ${spotlight.cityStateZip}</p>
-                        <p>${spotlight.adcopy}</p>`
-    mainspotlight.append(newdiv);    
-  })
-
-}
-
-async function getBusinessData() {
-  const response = await fetch(businessDataUrl);
-  if (response.ok) {
-    const data = await response.json();
-    displaySpotlights(data.businesses);
-  } else {
-    console.error("There was an error loading the data.");
-  }
-}
-
-getBusinessData();
